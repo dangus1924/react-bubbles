47b7278edec54e1c328733d7ad70e81684156ff4
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-11-09 13:16:44 -0500

    fixed spelling error in the private route file

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import BubblePage from './components/BubblePage';
 import Login from "./components/Login";
 import "./styles.scss";
+import PrivateRoute from "./components/Routes/PrivateRoute";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute path='/bubbles' component={BubblePage} />
       </div>
     </Router>
   );
